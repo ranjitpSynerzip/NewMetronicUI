6f59323e638c186460d11c742592df678f2f2207
ranjitpSynerzip <ranjit.patil@synerzip.com>
2019-12-27 16:48:52 +0530

    merged coad of fund allocation

--- src/app/views/pages/settings/fund-allocation/fund-allocation.component.ts
+++ src/app/views/pages/settings/fund-allocation/fund-allocation.component.ts
@@ -28,8 +28,24 @@ export class FundAllocationComponent implements OnInit, OnDestroy {
   clickedRow: any;
   clickedColumn: any;
 
+  PopupVisible = false;
+  PopupTitle = '';
+  CellValue = '';
+  seriseAmount = '';
+  seriseName = '';
+  fundName = '';
+  campusName = '';
+  
   constructor(private httpClient: HttpClient, private service: FundAllocationService) {
-    this.subscription = this.service.getfundAllocation().subscribe(
+
+  }
+
+  ngOnInit() {
+    this.getFundAllocation();
+  }
+
+  getFundAllocation() {
+    this.service.getfundAllocation().subscribe(
       data => {
         this.serviceData = data;
         this.pivotGridDataSource = {
@@ -53,65 +69,53 @@ export class FundAllocationComponent implements OnInit, OnDestroy {
             dataField: "seriesDetailAamount",
             dataType: "number",
             format: "currency",
-            summaryType:"sum",
+            summaryType: "sum",
             area: "data",
           }],
           store: this.serviceData,
         };
-
-      })
-
-
-
-    // this.dataSource = {
-    //   remoteOperations: true,
-    //   store: AspNetData.createStore({
-    //     key: "seriesDetaild",
-    //     loadUrl: "http://172.25.29.38:88/api/SeriesDetails/fundallocation"
-    //   }),
-    //   fields: [{
-    //     caption: "Campus",
-    //     dataField: "campusName",
-    //     sortBySummaryField: "seriesDetailAamount",
-    //     sortBySummaryPath: [],
-    //     sortOrder: "desc",
-    //     area: "row"
-    //   }, {
-    //     caption: "fundName",
-    //     dataField: "fundName",
-    //     dataType: "string",
-    //     area: "column"
-    //   },
-    //   {
-    //     caption: "Serise",
-    //     dataField: "seriesName",
-    //     dataType: "string",
-    //     area: "column"
-    //   }, {
-    //     caption: "seriesDetailAamount",
-    //     dataField: "seriesDetailAamount",
-    //     dataType: "number",
-    //     format: "currency",
-    //     area: "data",
-    //   }, {
-    //     caption: "Store",
-    //     dataField: "StoreName"
-    //   },]
-    // }
+      });
   }
-
-  ngOnInit() {
-
-  }
-
 
   onCellClick(e) {
-    console.log('onCellClick', e);
+    console.log('onCellClick', e)
+    if (e.area == "data" && e.cell.columnPath.length > 1) {
+      var rowPathLength = e.cell.rowPath.length,
+        rowPathName = e.cell.rowPath[rowPathLength - 1];
+      this.campusName = rowPathName;
+      this.fundName = e.cell.columnPath[0];
+      this.seriseName = e.cell.columnPath[1];
+      this.CellValue = e.cell.value;
+      this.PopupTitle = 'Update Amount';
+      this.PopupVisible = true;
+    }
   }
 
 
   ngOnDestroy() {
-    this.subscription.unsubscribe();
+   // this.subscription.unsubscribe();
+  }
+
+  onPopupShown() {
+    console.log();
+  }
+
+  updateMyValue(e) {
+    console.log(e);
+  };
+
+
+  onSubmit(form) {
+    console.log(form.value);
+    this.seriseAmount = (Object.values(form.value).toLocaleString());
+    this.PopupVisible = false;
+    this.service.updateseriesdetailamount(this.seriseAmount, this.seriseName, this.fundName, this.campusName).subscribe(
+      success => { console.log('updateseriesdetailamount'); 
+      this.getFundAllocation();
+ 
+     },
+      error => { console.log('error'); }
+    );
   }
 
 
