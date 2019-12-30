33c3914153cad1b5e31568fd0136084528b98608
ranjitpSynerzip <ranjit.patil@synerzip.com>
2019-12-30 10:28:42 +0530

    Send extra params

--- src/app/shared/Services/fund-allocation.service.ts
+++ src/app/shared/Services/fund-allocation.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 
 import { FundallocationModel } from '../models/fund-amount.model';
@@ -16,112 +16,112 @@ export class CampusSummary {
 
 let campusSummary: CampusSummary[] = [
     {
-    "campusId": 1,
-    "campusName": "Australia",
-    "seriesName": "seriesName1",
-    "Amount": 3920,
-    "seriesId": 1,
-    "displayOrder": "1"
-}, {
-    "campusId": 2,
-    "campusName": "Australia",
-    "seriesName": "seriesName1",
-    "Amount": 6040,
-    "seriesId": 2,
-    "displayOrder" : "1"
-}, {
-    "campusId": 3,
-    "campusName": "Australia",
-    "seriesName": "seriesName2",
-    "Amount": 2205,
-    "seriesId":3,
-    "displayOrder": "2"
-}, {
-    "campusId": 4,
-    "campusName": "Australia",
-    "seriesName": "seriesName2",
-    "Amount": 990,
-    "seriesId": 4,
-    "displayOrder": "2"
-}, {
-    "campusId": 5,
-    "campusName": "Australia1",
-    "seriesName": "seriesName2",
-    "Amount": 700,
-    "seriesId": 5,
-    "displayOrder": "2"
-}, {
-    "campusId": 6,
-    "campusName": "Australia1",
-    "seriesName": "seriesName1",
-    "Amount": 2325,
-    "seriesId": 6,
-    "displayOrder": "1"
-}, {
-    "campusId": 7,
-    "campusName": "Australia1",
-    "seriesName": "seriesName1",
-    "Amount": 1980,
-    "seriesId": 7,
-    "displayOrder": "1"
-}, {
-    "campusId": 8,
-    "campusName": "Australia1",
-    "seriesName": "seriesName1",
-    "Amount": 1110,
-    "seriesId": 8,
-    "displayOrder": "1"
-}, {
-    "campusId": 9,
-    "campusName": "Australia1",
-    "seriesName": "seriesName2",
-    "Amount": 3090,
-    "seriesId": 9,
-    "displayOrder": "2"
-}, {
-    "campusId": 10,
-    "campusName": "Australia1",
-    "seriesName": "seriesName2",
-    "Amount": 1640,
-    "seriesId": 10,
-    "displayOrder": "2"
-}, {
-    "campusId": 11,
-    "campusName": "Australia2",
-    "seriesName": "seriesName2",
-    "Amount": 3190,
-    "seriesId": 11,
-    "displayOrder": "2"
-}, {
-    "campusId": 12,
-    "campusName": "Australia2",
-    "seriesName": "seriesName2",
-    "Amount": 3250,
-    "seriesId": 12,
-    "displayOrder": "2"
-}, {
-    "campusId": 13,
-    "campusName": "Australia2",
-    "seriesName": "seriesName1",
-    "Amount": 5550,
-    "seriesId": 13,
-    "displayOrder": "1"
-}, {
-    "campusId": 14,
-    "campusName": "Australia2",
-    "seriesName": "seriesName1",
-    "Amount": 2860,
-    "seriesId": 14,
-    "displayOrder": "1"
-},
-{
-    "campusId": 15,
-    "campusName": "Australia2",
-    "seriesName": "seriesName1",
-    "Amount": 990,
-    "seriesId": 15,
-    "displayOrder": "1"
-}];
+        "campusId": 1,
+        "campusName": "Australia",
+        "seriesName": "seriesName1",
+        "Amount": 3920,
+        "seriesId": 1,
+        "displayOrder": "1"
+    }, {
+        "campusId": 2,
+        "campusName": "Australia",
+        "seriesName": "seriesName1",
+        "Amount": 6040,
+        "seriesId": 2,
+        "displayOrder": "1"
+    }, {
+        "campusId": 3,
+        "campusName": "Australia",
+        "seriesName": "seriesName2",
+        "Amount": 2205,
+        "seriesId": 3,
+        "displayOrder": "2"
+    }, {
+        "campusId": 4,
+        "campusName": "Australia",
+        "seriesName": "seriesName2",
+        "Amount": 990,
+        "seriesId": 4,
+        "displayOrder": "2"
+    }, {
+        "campusId": 5,
+        "campusName": "Australia1",
+        "seriesName": "seriesName2",
+        "Amount": 700,
+        "seriesId": 5,
+        "displayOrder": "2"
+    }, {
+        "campusId": 6,
+        "campusName": "Australia1",
+        "seriesName": "seriesName1",
+        "Amount": 2325,
+        "seriesId": 6,
+        "displayOrder": "1"
+    }, {
+        "campusId": 7,
+        "campusName": "Australia1",
+        "seriesName": "seriesName1",
+        "Amount": 1980,
+        "seriesId": 7,
+        "displayOrder": "1"
+    }, {
+        "campusId": 8,
+        "campusName": "Australia1",
+        "seriesName": "seriesName1",
+        "Amount": 1110,
+        "seriesId": 8,
+        "displayOrder": "1"
+    }, {
+        "campusId": 9,
+        "campusName": "Australia1",
+        "seriesName": "seriesName2",
+        "Amount": 3090,
+        "seriesId": 9,
+        "displayOrder": "2"
+    }, {
+        "campusId": 10,
+        "campusName": "Australia1",
+        "seriesName": "seriesName2",
+        "Amount": 1640,
+        "seriesId": 10,
+        "displayOrder": "2"
+    }, {
+        "campusId": 11,
+        "campusName": "Australia2",
+        "seriesName": "seriesName2",
+        "Amount": 3190,
+        "seriesId": 11,
+        "displayOrder": "2"
+    }, {
+        "campusId": 12,
+        "campusName": "Australia2",
+        "seriesName": "seriesName2",
+        "Amount": 3250,
+        "seriesId": 12,
+        "displayOrder": "2"
+    }, {
+        "campusId": 13,
+        "campusName": "Australia2",
+        "seriesName": "seriesName1",
+        "Amount": 5550,
+        "seriesId": 13,
+        "displayOrder": "1"
+    }, {
+        "campusId": 14,
+        "campusName": "Australia2",
+        "seriesName": "seriesName1",
+        "Amount": 2860,
+        "seriesId": 14,
+        "displayOrder": "1"
+    },
+    {
+        "campusId": 15,
+        "campusName": "Australia2",
+        "seriesName": "seriesName1",
+        "Amount": 990,
+        "seriesId": 15,
+        "displayOrder": "1"
+    }];
 
 
 @Injectable({
@@ -160,8 +160,14 @@ export class FundAllocationService {
     }
 
     updateseriesdetailamount(amount: string, seriseName: string, fundName: string, campusName: string): Observable<any> {
+        // let params = new HttpParams();
+        // params = params.append('amount' , amount );
+        // params = params.append('seriesName' , seriseName );
+        // params = params.append('fundName' , fundName );
+        // params = params.append('campusName' , campusName );
         const httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/json' }) };
-        return this.http.put<any>(this.baseUrl + '/SeriesDetails/updateseriesdetailamount?amount=' + amount +'&seriesName='+ seriseName +'&fundName='+ fundName +'&campusName='+ campusName, httpOptions)
+
+        return this.http.put<any>(this.baseUrl + '/SeriesDetails/updateseriesdetailamount?amount=' + amount + '&seriesName=' + seriseName + '&fundName=' + fundName + '&campusName=' + campusName, httpOptions)
     }
 
 
