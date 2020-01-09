11fa5bfc5dd078d7a60ccb66c54fbe8584620dda
ranjitpSynerzip <ranjit.patil@synerzip.com>
2020-01-09 12:34:22 +0530

    Added missing

---
+++ src/app/views/partials/content/widgets/barchart-widget/barchart-widget.component.ts
@@ -0,0 +1,90 @@
+import { Component, OnInit, ElementRef, Input, ViewChild } from '@angular/core';
+import { LayoutConfigService } from '../../../../../core/_base/layout/services/layout-config.service';
+
+
+@Component({
+	selector: 'kt-barchart-widget',
+	templateUrl: './barchart-widget.component.html',
+	styleUrls: ['./barchart-widget.component.scss']
+})
+export class BarchartWidgetComponent implements OnInit {
+	@Input() title: string;
+	@Input() desc: string;
+	@Input() data: { labels: string[]; datasets: any[] };
+	@ViewChild('chart', { static: true }) chart: ElementRef;
+
+	constructor(private layoutConfigService: LayoutConfigService) { }
+
+	ngOnInit() {
+		console.log('title', this.title);
+		console.log('desc', this.desc);
+		console.log('data', this.data);
+		debugger;
+		if (!this.data) {
+			this.data = {
+				labels: this.data.labels,
+				datasets: [
+					{
+						// label: 'dataset 1',
+						backgroundColor: this.layoutConfigService.getConfig(
+							'colors.state.success'
+						),
+						data: this.data.datasets
+					}
+				]
+			};
+		}
+
+		this.initChartJS();
+	}
+
+	/** Init chart */
+	initChartJS() {
+		const chart = new Chart(this.chart.nativeElement, {
+			type: 'bar',
+			data: this.data,
+			options: {
+				title: {
+					display: false
+				},
+				tooltips: {
+					intersect: false,
+					mode: 'nearest',
+					xPadding: 10,
+					yPadding: 10,
+					caretPadding: 10
+				},
+				legend: {
+					display: false
+				},
+				responsive: true,
+				maintainAspectRatio: false,
+				barRadius: 4,
+				scales: {
+					xAxes: [
+						{
+							display: false,
+							gridLines: false,
+							stacked: true
+						}
+					],
+					yAxes: [
+						{
+							display: false,
+							stacked: true,
+							gridLines: false
+						}
+					]
+				},
+				layout: {
+					padding: {
+						left: 0,
+						right: 0,
+						top: 0,
+						bottom: 0
+					}
+				}
+			}
+		});
+	}
+}
